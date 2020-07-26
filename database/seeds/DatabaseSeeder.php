fd0923e4b7784d05f0aea4867539df6de336a45d
Dauda Baba <dbaba4dev@gmail.com>
2020-07-26 11:17:43 +0100

    Display all Questions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
 
-        factory(App\User::class, 3)->create()->each(function ($u){
+        factory(App\User::class, 5)->create()->each(function ($u){
             $u->questions()
-                ->saveMany(factory(App\Question::class, rand(1,5))->make()
+                ->saveMany(factory(App\Question::class, rand(1,10))->make()
                 );
         });
 
